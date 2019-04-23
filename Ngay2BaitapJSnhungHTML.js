5ac86d1fc1cf7b25f63218e7a7c006a45c551af3
tranthanhhieuthao <tranthanhhieuthao@gmail.com>
2019-04-23 17:21:50 +0700

    baitap

---
+++ Ngay2BaitapJSnhungHTML.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth=prompt("Enter the width");
+inputheight=prompt("Enter the height");
+let width=parseInt(inputwidth);
+let height=parseInt(inputheight);
+let Area=width * height;
+document.write("Area= " +Area);
